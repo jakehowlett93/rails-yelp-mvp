92e5504747923d0b15b496db3ea8e98ac8a45061
jake howlett <jake.howlett93@gmail.com>
2021-02-17 12:43:42 +0000

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :restaurants, except: %i[destroy update] do
+    resources :reviews, only: %i[index new]
+  end
 end
