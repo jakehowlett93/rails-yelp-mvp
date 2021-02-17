53429c489176883d50d21dc52f241e46fe2a1227
jake howlett <jake.howlett93@gmail.com>
2021-02-17 12:52:59 +0000

    add restaurant index method and view

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,17 @@
 class RestaurantsController < ApplicationController
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+  
+  end
+
+  def new
+  
+  end
+
+  def create
+  
+  end
 end
