3d189f16a8b544325fcd8c12e059e54fab7ed936
jake howlett <jake.howlett93@gmail.com>
2021-02-17 13:23:04 +0000

    add review generation to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,10 +3,17 @@ Restaurant.destroy_all
 
 puts 'Creating restaurants...'
 5.times do
-  Restaurant.create(
+  restaurant = Restaurant.new(
     name: Faker::Restaurant.name,
     address: Faker::Address.full_address,
     phone_number: Faker::PhoneNumber.cell_phone,
     category: %w[chinese italian japanese french belgian].sample
   )
+  restaurant.save
+  review = Review.new(
+    content: Faker::GreekPhilosophers.quote,
+    rating: [0, 1, 2, 3, 4, 5].sample,
+    restaurant_id: restaurant.id
+  )
+  review.save
 end
