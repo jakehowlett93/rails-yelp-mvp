4342fed20837bca92b336fad33b09035a16941c3
jake howlett <jake.howlett93@gmail.com>
2021-02-17 13:46:57 +0000

    create the create method

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -13,12 +13,17 @@ class RestaurantsController < ApplicationController
   end
 
   def create
-    
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
   end
 
   private
 
-  # def find_restaurant
-  #   @restaurant = Restaurant.find([params[:id]])
-  # end
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
 end
